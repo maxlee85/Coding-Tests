b5e2eea5cf484cc60caa23218d846b3fe2c2c699
maxlee85 <maxwellclee@gmail.com>
2021-08-21 15:34:21 -0400

    Update practice.py

--- practice.py
+++ practice.py
@@ -49,22 +49,3 @@ class Solution:
             if nums[i-1] >= 0:
                 nums[i]+=nums[i-1]
         return max(nums)
-    
-"""
-Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-
-Given nums = [2, 7, 11, 15], target = 9,
-
-Because nums[0] + nums[1] = 2 + 7 = 9,
-return [0, 1].
-"""    
-
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        my_dict = {}
-        for index, value in enumerate(nums):
-            comp = target - value
-            if comp in my_dict:
-                return [index, my_dict[comp]]
-            else:
-                my_dict[value] = index
