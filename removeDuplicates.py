3c1bcd594e4e62fbd67ac42ca2c7c1020a244c64
maxlee85 <maxwellclee@gmail.com>
2021-08-21 15:35:51 -0400

    Create removeDuplicates.py

---
+++ removeDuplicates.py
@@ -0,0 +1,21 @@
+"""
+Challenge:
+Given a sorted array nums, remove the duplicates in-place such that each element appears only once and return the new length.
+"""
+
+"""
+Example: 
+[0,0,1,1,1,2,2,3,3,4] 
+returns 5
+"""
+
+# remove dupes
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i = 0
+        while i < len(nums)-1:
+            if nums[i] == nums[i+1]:
+                nums.remove(nums[i+1])
+            else:
+                i+=1
+        return len(nums)
