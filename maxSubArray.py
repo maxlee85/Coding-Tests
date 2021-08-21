5a70f26710754651831cbcfc2767b885e4555392
maxlee85 <maxwellclee@gmail.com>
2021-08-21 15:36:31 -0400

    Update and rename practice.py to maxSubArray.py

--- maxSubArray.py
+++ maxSubArray.py
@@ -1,22 +1,10 @@
 """
-Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
-
-[0,0,1,1,1,2,2,3,3,4] returns 5
-"""
-
-# remove dupes
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        i = 0
-        while i < len(nums)-1:
-            if nums[i] == nums[i+1]:
-                nums.remove(nums[i+1])
-            else:
-                i+=1
-        return len(nums)
-"""
+Challenge:
 Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+"""
 
+"""
+Example:
 Input: [-2,1,-3,4,-1,2,1,-5,4],
 Output: 6
 
@@ -42,7 +30,8 @@ Iterate over array, if previous element is positive add that to current element.
 ->  [-2,1,-2,4,3,5,6,1,4] # add 1 to 4
                        ^
 ->  [-2,1,-2,4,3,5,6,1,5]
-"""                
+"""           
+
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         for i in range(1, len(nums)):
